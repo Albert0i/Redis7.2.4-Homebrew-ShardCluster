d587fe1e85998ac13028ac43371693aa1d914094
Alberto Iong <albert0i@hotmail.com>
2024-07-04 15:51:28 +0800

    add test.js

--- src/test.js
+++ src/test.js
@@ -2,36 +2,37 @@
 import 'dotenv/config'
 import { Redis } from "ioredis"
 
-const redis = new Redis(
-  {
-    port: 6379, // Redis port
-    host: "127.0.0.1", // Redis host
-    username: process.env.AUTH_USER, // needs Redis >= 6
-    password: process.env.AUTH_PASS,
-    db: 0, // Defaults to 0,
-    enableAutoPipelining: true, 
-    showFriendlyErrorStack: true
-  }
-);
+  const cluster = new Redis.Cluster([
+      { host: "127.0.0.1",  port: 6379 }, 
+      { host: "127.0.0.1",  port: 6380 },
+      { host: "127.0.0.1",  port: 6381 }
+      ], 
+      {
+        scaleReads: "slave",
+        redisOptions: {
+          username: process.env.AUTH_USER,
+          password: process.env.AUTH_PASS
+        }
+      });
 
   // Always start with 0
-  await redis.set("counter", 0);
+  await cluster.set("counter", 0);
 
   // Repeat for 1 hour
   for (let i=1; i<=3600; i++) {
     try {
       // Update the value 
-      await redis.incr("counter");
+      await cluster.incr("counter");
       
       // Read it back and display
-      console.log(`counter=${await redis.get('counter')}`)
+      console.log(`counter=${await cluster.get('counter')}`)
     } 
     catch (e) {  console.log('.'); }
     // Delay for 1 second
     await new Promise(resolve => setTimeout(resolve, 1000));
   }
 
-  await redis.disconnect()
+  await cluster.disconnect()
 
   /*
      ioredis
